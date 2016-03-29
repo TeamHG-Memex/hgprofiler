cc696ba14fdf8fbdd42e382e79fbd7bc9d4a4a72
Luke Maxwell <luke@codepunk.xyz>
2016-03-29 20:43:44 +0100

    Rename wsgi file

--- application.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
-
-def run(environ, start_response):
-    import app
-    application = app.bootstrap()
-    return application
-
