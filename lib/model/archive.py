b5d7275e83cb9a0659e72cc90f7d9e05a9d8bd73
Luke Maxwell <lukemaxwellshouse@gmail.com>
2016-04-05 13:36:39 +0100

    Add zip_file_url to archive model as_dict method.

--- lib/model/archive.py
+++ lib/model/archive.py
@@ -8,8 +8,6 @@ from sqlalchemy import (DateTime,
 from sqlalchemy.orm import relationship
 
 from model import Base
-#from model import Group
-#from model import File
 
 
 class Archive(Base):
@@ -24,14 +22,12 @@ class Archive(Base):
     job_id = Column(String(255), nullable=False)
     username = Column(String(255), nullable=False)
     group_id = Column(Integer, ForeignKey('group.id'), nullable=True)
-    #group = relationship('Group', foreign_keys='Archive.group_id', lazy='joined')
     date = Column(DateTime, default=func.current_timestamp())
     site_count = Column(Integer, nullable=False)
     found_count = Column(Integer, nullable=False)
     not_found_count = Column(Integer, nullable=False)
     error_count = Column(Integer, nullable=False)
     zip_file_id = Column(Integer, ForeignKey('file.id', name='fk_zip_file'))
-    #zip_file = relationship('File', primaryjoin='Archive.zip_file_id == File.id')
 
     def __init__(self,
                  job_id,
@@ -55,22 +51,17 @@ class Archive(Base):
 
     def as_dict(self):
         ''' Return dictionary representation of this archive. '''
-        #if self.group is not None:
-        #    group = self.group.as_dict()
-        #else:
-        #    group = None
 
         return {
             'id': self.id,
             'job_id': self.job_id,
             'username': self.username,
-            #'group': group,
             'group_id': self.group_id,
             'date': self.date.isoformat(),
             'site_count': self.site_count,
             'found_count': self.found_count,
             'not_found_count': self.not_found_count,
             'error_count': self.error_count,
-            #'zip_file_url': '/api/file/{}'.format(self.zip_file.id)
+            'zip_file_url': '/api/file/{}'.format(self.zip_file.id)
             'zip_file_id': self.zip_file_id
         }
