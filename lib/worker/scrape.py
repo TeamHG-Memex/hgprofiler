de504009f5e5ee7267e897c7acfc92a1c3d97e34
Luke Maxwell <lukemaxwellshouse@gmail.com>
2016-04-06 14:54:38 +0100

    Increase tornado max_clients.

--- lib/worker/scrape.py
+++ lib/worker/scrape.py
@@ -19,6 +19,7 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) '\
              'Gecko/20100101 Firefox/40.1'
 
 #httpclient.AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
+httpclient.AsyncHTTPClient.configure(None, max_clients=100)
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
